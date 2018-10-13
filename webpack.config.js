2b4bb02c8909c350df1e4d76431f1dfeb31ddb4e
Mauricio Flores <contact@maurojflores.com>
2018-10-13 17:19:44 -0600

    index and bundle moved to same directory (public); set up webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,15 @@ const path = require('path');
 module.exports = {
     entry: './src/js/main.js',
     output: {
-        path: path.join( __dirname, '/public/dist' ),
+		path: path.join( __dirname, '/public' ),
         filename: 'bundle.js'
-    },
+	},
+	devtool: 'source-map',
+	devServer: {
+		contentBase: './public',
+		port: 9000,
+		watchContentBase: true
+	},
     module: {
         rules: [
             {
